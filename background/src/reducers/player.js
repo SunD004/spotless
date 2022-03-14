3087f7f7fc50c85de275e322542c439e6f34d6f3
corentin|SunD <corentin.calvo@epitech.eu>
2022-03-14 11:32:53 +0100

    new version adjust timer

--- background/src/reducers/player.js
+++ background/src/reducers/player.js
@@ -1,7 +1,7 @@
 import { PLAYER as actionTypes } from '../constants/actionTypes'
 import { TRACK as trackActionTypes } from '../constants/actionTypes'
 
-const COUNTDOWN = 28
+const COUNTDOWN = 29
 
 const defaultState = {
   playback: null,
